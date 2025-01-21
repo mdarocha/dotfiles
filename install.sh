8404a934c0fef760c093c6df89ae79e64f2ba54c
mdarocha <git@mdarocha.pl>
2025-01-21 17:21:08 +0100

    Tweak nix to work in codespaces properly

--- install.sh
+++ install.sh
@@ -14,6 +14,14 @@ if ! command -v nix &> /dev/null; then
     if [[ "$CODESPACES" == "true" ]]; then
         echo "🔨 Installing Nix for Codespaces..."
         sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+        echo 'if [ -e /home/codespace/.nix-profile/etc/profile.d/nix.sh ]; then . /home/codespace/.nix-profile/etc/profile.d/nix.sh; fi # added by dotfiles, required for Nix to work over ssh' >> ~/.bashrc
+
+        sudo mkdir -p /etc/nix
+        sudo touch /etc/nix/nix.conf
+        echo 'sandbox = false' | sudo tee -a /etc/nix/nix.conf
+        echo 'experimental-features = nix-command flakes' | sudo tee -a /etc/nix/nix.conf
+        echo 'extra-platforms = aarch64-linux arm-linux' | sudo tee -a /etc/nix/nix.conf
     else
         echo "🔨 Installing Nix..."
         curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
