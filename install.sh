924e12881f21799abd2de2debf6c14b7f6ee7235
mdarocha <git@mdarocha.pl>
2025-01-21 17:09:46 +0100

    Single-user install on codespaces, add binfmt on codespaces

--- install.sh
+++ install.sh
@@ -3,12 +3,22 @@
 echo "Hello!"
 echo "======"
 
+if [[ "$CODESPACE" == "true" ]]; then
+    echo "Installing binfmt support..."
+    docker run --privileged --rm tonistiigi/binfmt --install arm64,arm
+fi
+
 echo "🔨 Installing Nix..."
 
 if ! command -v nix &> /dev/null; then
-    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
-      sh -s -- install --no-confirm
-    echo "🔨 Nix installed."
+    if [[ "$CODESPACE" == "true" ]]; then
+        echo "🔨 Installing Nix for Codespaces..."
+        sh <(curl -L https://nixos.org/nix/install) --no-daemon
+    else
+        echo "🔨 Installing Nix..."
+        curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+          sh -s -- install --no-confirm
+    fi
 else
     echo "🔨 Nix is already installed."
 fi
