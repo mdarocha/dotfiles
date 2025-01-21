cbd155a4113e55ec603e9037c9878150a8780a1e
mdarocha <git@mdarocha.pl>
2025-01-21 15:14:53 +0100

    Add Nix install

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Hello!"
+echo "======"
+
+echo "🔨 Installing Nix..."
+
+if ! command -v nix &> /dev/null; then
+    curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+      sh -s -- install
+    echo "🔨 Nix installed."
+else
+    echo "🔨 Nix is already installed."
+fi
