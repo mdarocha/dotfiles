eb25ffbfc09dabb2c7bc62fd0f6ad4a0cc5d580c
Marek Darocha <git@mdarocha.pl>
2025-10-23 14:30:27 +0000

    disable git.confirmSync

--- config/vscode/settings.nix
+++ config/vscode/settings.nix
@@ -6,6 +6,7 @@
       "terminal.integrated.defaultProfile.linux" = "zsh";
       "git.autofetch" = true;
       "git.enableSmartCommit" = true;
+      "git.confirmSync" = false;
     };
     extensions = [
       "bbenoist.nix"
