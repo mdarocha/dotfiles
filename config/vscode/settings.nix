72d1855f2050c646fd838f96aeea3fdc9a923692
Marek Darocha <git@mdarocha.pl>
2026-02-11 15:05:01 +0000

    updates

--- config/vscode/settings.nix
+++ config/vscode/settings.nix
@@ -9,6 +9,8 @@
       "git.confirmSync" = false;
       "github.copilot.nextEditSuggestions.enabled" = false;
       "editor.fontFamily" = "Hack Nerd Font Mono";
+      "terminal.integrated.minimumContrastRatio" = 1;
+      "workbench.colorTheme" = "Solarized Dark";
     };
     extensions = [
       "bbenoist.nix"
