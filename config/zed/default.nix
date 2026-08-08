2d0ecfb87b9cf4063c6c5d5ce2eaca654d544880
mdarocha <git@mdarocha.pl>
2026-08-08 14:51:28 +0000

    zed: fix false-positive overwrite warnings from key-order mismatch

--- config/zed/default.nix
+++ config/zed/default.nix
@@ -121,7 +121,7 @@ in
           --slurpfile old "$existing_json" \
           '($nix[0] // {} | keys_unsorted) as $nk |
            ($old[0] // {}) as $o | ($nix[0] // {}) as $n |
-           $nk[] | select($o[.] != null and ($o[.] | tojson) != ($n[.] | tojson))')
+           $nk[] | select($o[.] != null and $o[.] != $n[.])
 
         if [ -n "$overwritten" ]; then
           echo "WARNING: zed settings: Nix overwrote the following top-level keys from the existing settings.json (backup: $backup):"
