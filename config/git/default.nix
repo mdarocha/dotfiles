a41df6e3d815a5e4dc204f6f61bd12f9c12e7199
mdarocha <git@mdarocha.pl>
2026-08-17 20:10:35 +0000

    config/git: rewrite git@github.com: remotes to https via gh's credential helper

--- config/git/default.nix
+++ config/git/default.nix
@@ -1,11 +1,12 @@
 {
+  config,
   lib,
   pkgs,
   ...
 }:
 
 let
-  inherit (lib) mkDefault;
+  inherit (lib) mkDefault mkIf mkMerge;
 in
 {
   config.programs.gh = {
@@ -22,25 +23,34 @@ in
     ];
     lfs.enable = true;
 
-    settings = {
-      user.name = mkDefault "mdarocha";
-      user.email = mkDefault "git@mdarocha.pl";
+    settings = mkMerge [
+      {
+        user.name = mkDefault "mdarocha";
+        user.email = mkDefault "git@mdarocha.pl";
 
-      # additional settings
-      # mostly based on https://jvns.ca/blog/2024/02/16/popular-git-config-options
-      init.defaultBranch = "main";
-      pull.rebase = false;
-      push.autoSetupRemote = true;
-      merge.conflictstyle = "zdiff3";
-      commit.verbose = true;
-      rerere.enable = true;
-      diff.algorithm = "histogram";
+        # additional settings
+        # mostly based on https://jvns.ca/blog/2024/02/16/popular-git-config-options
+        init.defaultBranch = "main";
+        pull.rebase = false;
+        push.autoSetupRemote = true;
+        merge.conflictstyle = "zdiff3";
+        commit.verbose = true;
+        rerere.enable = true;
+        diff.algorithm = "histogram";
 
-      # ghq tool config
-      ghq = {
-        root = lib.mkDefault "~/Projekty";
-      };
-    };
+        # ghq tool config
+        ghq = {
+          root = lib.mkDefault "~/Projekty";
+        };
+      }
+      # gh's credential helper only ever handles HTTPS auth; rewrite SSH
+      # remotes to HTTPS wherever it's configured, so `git push`/`fetch` work
+      # through it instead of requiring an `ssh` binary and key/agent setup
+      # (notably absent in the agent sandbox).
+      (mkIf config.programs.gh.enable {
+        url."https://github.com/".insteadOf = "git@github.com:";
+      })
+    ];
 
     # include a machine-local config if available
     includes = [
